eafd3d1b3819bb5a2c664d20d9c18b12e6cc65f1
bibek gautam <bibek@awecode.com>
2023-06-15 17:30:11 +0545

    docs: docstring for Generator class

--- src/generator.py
+++ src/generator.py
@@ -1,20 +1,36 @@
 
 class Generator():
+    ''' Generates a series of questions from the given paragraph or url or topic.
 
-    def __init__(self, par=None, url=None, topic=None, type='word'):
+        Usage:
+
+        ```python
+            from src import Generator
+            generator = Generator(par=par)
+        ```
+        Possible arguments:
+        - par : paragraph to generate questions from (defaults to None)
+        - url : url of the source based on which to generate questions (defaults to None)
+        - topic : topic on which to generate questions; the algorithm will search the web and generate questions (defaults to None)
+        - type : 'word' or 'sentence', defaults to 'word'
+    '''
+
+    def __init__(self, par:str=None, url:str=None, topic:str=None, type='word'):
         self.par = par
         self.url = url
         self.topic = topic
         self.type = type
 
     def generate(self):
+        if not (self.par or self.url or self.topic):
+            return("Cannot generate. You must provide one parameter among (par, url, topic).")
         par = self.par
         if self.type == 'word':
             tokens = self.word_tokenize(par)
         elif self.type == 'sentence':
             tokens = self.sentence_tokenize(par)
         else:
-            print('Invalid type. Valid types are "word" and "sentence".')
+            return('Invalid type. Valid types are "word" and "sentence".') 
         return tokens 
     
     def word_tokenize(self, text):
