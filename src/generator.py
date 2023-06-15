5f9001f4e1563bc5d304697cde2cb2ee28dc7a89
bibek gautam <bibek@awecode.com>
2023-06-15 17:13:46 +0545

    feat: tokenize paragraphs

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,26 @@
 
 class Generator():
-    def generate(par: str = None, url: str = None, top: str = None):
+
+    def __init__(self, par=None, url=None, topic=None, type='word'):
+        self.par = par
+        self.url = url
+        self.topic = topic
+        self.type = type
+
+    def generate(self):
+        par = self.par
+        if self.type == 'word':
+            tokens = self.word_tokenize(par)
+        elif self.type == 'sentence':
+            tokens = self.sentence_tokenize(par)
+        else:
+            print('Invalid type. Valid types are "word" and "sentence".')
+        return tokens 
     
-        return 'Hello' 
+    def word_tokenize(self, text):
+        tokens = text.strip().split(' ')
+        return tokens
+    
+    def sentence_tokenize(self, text):
+        tokens = text.strip().split('.')
+        return tokens
